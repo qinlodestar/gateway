97c2dcbfa35b23d86556e5e6071f2c840d3c015f
yongbo.qin <yongbo.qin@91sports.cc>
2019-12-12 17:39:08 +0800

    test

--- lua/conf/config.test.lua
+++ lua/conf/config.test.lua
@@ -1,6 +1,6 @@
 local _M = {}
     function _M.redisconf()
-        local redis_conf = {["ip"] = "127.0.0.",["port"] = "6379",["passwd"] = "123456"}
+        local redis_conf = {["ip"] = "127.0.0.1",["port"] = "6379",["passwd"] = "123456"}
         return redis_conf
     end
 return _M
